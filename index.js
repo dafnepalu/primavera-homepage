f1dbff6ba424bac457f1069fce691e42ed6f6b1d
Dafne Palú <paludafne@gmail.com>
2022-05-24 13:01:15 +0100

    Corrected the Login form's Submit mechanism

--- index.js
+++ index.js
@@ -11,45 +11,50 @@ document.querySelector("#telaLogin span").addEventListener("click", function () 
 });
 
 // Lidar com os erros do formulário de login:
-function submeterFormularioLogin(){
-    let erros = document.querySelector("#erros");
-    let erroEmail = document.querySelector("#erroEmail");
-    let erroUtilizador = document.querySelector("#erroUtilizador");
-    
-    document.querySelector("#formularioLogin").addEventListener("submit", function (evento) {
+let erros = document.querySelector("#erros");
+let erroEmail = document.querySelector("#erroEmail");
+let erroUtilizador = document.querySelector("#erroUtilizador");
+
+let campoEmail = document.querySelector("#email");
+let campoSenha = document.querySelector("#senha");
+
+document.querySelector("#formularioLogin").addEventListener("submit", function (evento) {
+    submeterFormularioLogin(evento);
+});
+
+campoEmail.addEventListener("keyup", function (evento) {
+    submeterFormularioLoginComEnter(evento)
+});
+
+campoSenha.addEventListener("keyup", function (evento) {
+    submeterFormularioLoginComEnter(evento)
+});
+
+function submeterFormularioLoginComEnter(evento) {
+    if (evento.code === "Enter" || evento.code === "NumpadEnter") {
         evento.preventDefault();
-        let campoEmail = document.querySelector("#email").value;
-        let campoSenha = document.querySelector("#senha").value;
-    
-        if (campoEmail === "" || campoSenha === "") {
-            erros.innerHTML = "<p>Os dois campos são de preenchimento obrigatório!</p>"
-        } else {
-    
-            // O padrão regex abaixo pretende abarcar os endereços de email que contenham pelo menos um caractere e no máximo 64 antes do símbolo do @; pelo menos um caractere e no máximo 255 entre o @ e o ponto; pelo menos dois caracteres depois do ponto.
-            let padraoEmail = /^\S{1,64}[@]\S{1,255}[.]\S{2,}$/
-    
-            if (!padraoEmail.test(campoEmail)) {
-                erroEmail.textContent = "O formato do endereço de e-mail não está correto.";
-                erroEmail.style.display = "block";
-            } else {
-                erroEmail.style.display = "none";
-            }
-        }
-    });
-    console.log("rodou");
+        submeterFormularioLogin(evento);
+    }
 }
 
-// Permitir submeter o formulário ao clicar na tecla Enter em qualquer um dos dois campos do formulário:
-document.getElementById("email").addEventListener("keyup", function(evento){
-    if (evento.code === "Enter"){
-        evento.preventDefault();
-        submeterFormularioLogin();
-    }
-});
+function submeterFormularioLogin(evento) {
+    evento.preventDefault();
+    let emailValor = campoEmail.value;
+    let senhaValor = campoSenha.value;
 
-document.getElementById("senha").addEventListener("keyup", function(evento){
-    if (evento.code === "NumpadEnter"){
-        evento.preventDefault();
-        submeterFormularioLogin();
+    if (emailValor === "" || senhaValor === "") {
+        erros.innerHTML = "<p>Os dois campos são de preenchimento obrigatório!</p>"
+    } else {
+
+        // O padrão regex abaixo pretende abarcar os endereços de email que contenham pelo menos um caractere e no máximo 64 antes do símbolo do @; pelo menos um caractere e no máximo 255 entre o @ e o ponto; pelo menos dois caracteres depois do ponto.
+        let padraoEmail = /^\S{1,64}[@]\S{1,255}[.]\S{2,}$/
+
+        if (!padraoEmail.test(emailValor)) {
+            erroEmail.textContent = "O formato do endereço de e-mail não está correto.";
+            erroEmail.style.display = "block";
+        } else {
+            erroEmail.style.display = "none";
+        }
     }
-});
+    console.log("Rodou");
+}
